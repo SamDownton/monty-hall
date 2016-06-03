984ce3398f3c231e883d33c83be7d3d0e6125e10
samuel.downton <samuel.downton@metoffice.gov.uk>
2016-06-03 15:17:11 +0100

    Various improvements

--- montyhall/montyhall/play.py
+++ montyhall/montyhall/play.py
@@ -41,55 +41,61 @@ def play_quietly(num_doors):
     shut_door = leave_door_shut(doors, door)
     # print "switched door", doors[shut_door]
     if doors[shut_door] == 1:
-        won = True
-    else: 
-        won = False
-    if won == True:
         return 1
-    else:
+    else: 
         return 0
-        
+    
 def play_loads_of_times(time_to_play, num_doors):
     i = 0
     results = []
     while i < time_to_play:
         result = play_quietly(num_doors)
-        print result
         results.append(result)
         i += 1
     percent_times_one = (float(sum(results)) / float(time_to_play)) * 100
     print percent_times_one, "% of games won"
     
 def play_interactively():
-    num_doors = int(raw_input("How many doors would you like to play with? : "))     
-    doors = hide_the_car(num_doors)
-    print "Please choose the door you think contains the prize ( 0 -", len(doors) - 1, "):"
-    door = int(raw_input())
-    closed_door = leave_door_shut(doors, door)
-    print "Goats have been revealed behind all the doors, except for your door, number", door
-    print "and door number", closed_door, ". Would you like to swap or keep your original choice (S/K)? "
-    choice = raw_input()
-    for i in xrange(len(doors)):
-        if doors[i] == 1:
-            prize_location = i
-    validate = False
-    while not validate:
-        if choice == "S" or choice =="K":
-            validate = True
+    play = True
+    while play:
+        num_doors = int(raw_input("How many doors would you like to play with? : "))     
+        doors = hide_the_car(num_doors)
+        print "Please choose the door you think contains the prize ( 0 -", len(doors) - 1, "):"
+        door = int(raw_input())
+        closed_door = leave_door_shut(doors, door)
+        print "Goats have been revealed behind all the doors, except for your door, number", door
+        print "and door number", closed_door, ". Would you like to swap or keep your original choice (S/K)? "
+        choice = raw_input()
+        for i in xrange(len(doors)):
+            if doors[i] == 1:
+                prize_location = i
+        validate = False
+        while not validate:
+            if choice == "S" or choice =="K":
+                validate = True
+            else:
+                choice = raw_input("You must select \"S\" or \"K\"")
+        if choice == "S":
+            door = closed_door
+        if doors[door] == 1:
+            print "Congratulations! You Win!"
         else:
-            choice = raw_input("You must select \"S\" or \"K\"")
-    if choice == "S":
-        door = closed_door
-    if doors[door] == 1:
-        print "Congratulations! You Win!"
-    else:
-        print "You lose! The prize was behind door number", prize_location
+            print "You lose! The prize was behind door number", prize_location
+        validate = False
+        continue_ = raw_input("Would you like to play again? (Y/N)")
+        while not validate:       
+            if continue_ == "Y" or continue_ == "N":
+                validate = True
+            else:
+                continue_ = raw_input("You must enter \"Y\" or \"N \"")
+        if continue_ == "N":
+            play = False
         
 def start_up():
     validated = False
-    choice = int(raw_input("Please enter 1 to play interactively or 2 to run a bulk simulation: "))
+    choice = raw_input("Please enter 1 to play interactively or 2 to run a bulk simulation: ")
     while not validated:
-        if choice == 1 or 2 or 3:
+        if int(choice) == 1 or int(choice) == 2 or int(choice) == 3:
             validated = True
         else:
             choice = int(raw_input("You must enter 1 or 2! :"))
@@ -97,7 +103,7 @@ def start_up():
         play_interactively()
     else:
         num_of_plays = int(raw_input("Enter number of times to play: "))
-        num_of_doors = int(raw_input("Enter number of doors in game: "))
+        num_of_doors = int(raw_input("Enter number of doors in game (2+): "))
         play_loads_of_times(num_of_plays, num_of_doors) 
          
 
